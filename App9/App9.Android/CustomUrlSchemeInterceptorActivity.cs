eed34028aaf9c47df399166849d00acf26fb4270
AlexandrMazharov <55503710+AlexandrMazharov@users.noreply.github.com>
2020-03-18 18:04:34 +0400

    Добавьте файлы проекта.

---
+++ App9/App9.Android/CustomUrlSchemeInterceptorActivity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using System;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+
+
+using OAuthNativeFlow;
+
+namespace App9.Droid
+{
+    //public class CustomUrlSchemeInterceptorActivity : ContentPage
+    //{
+		[Activity(Label = "CustomUrlSchemeInterceptorActivity", NoHistory = true, LaunchMode = LaunchMode.SingleTop)]
+		[IntentFilter(
+			new[] { Intent.ActionView },
+			Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
+			DataSchemes = new[] { "com.googleusercontent.apps.1012719202452-g8cgcjg3p0maua9skuh84t982q8i8sts" },
+			DataPath = "/oauth2redirect")]
+		public class CustomUrlSchemeInterceptorActivity : Activity
+		{
+			protected override void OnCreate(Bundle savedInstanceState)
+			{
+				base.OnCreate(savedInstanceState);
+
+				// Convert Android.Net.Url to Uri
+				var uri = new Uri(Intent.Data.ToString());
+
+				// Load redirectUrl page
+				AuthenticationState.Authenticator.OnPageLoading(uri);
+				var intent = new Intent(this, typeof(MainActivity));
+				intent.SetFlags(ActivityFlags.ClearTop | ActivityFlags.SingleTop);
+				StartActivity(intent);
+
+				this.Finish();
+
+				return;
+			}
+		}
+	}
---
+++ App9/App9/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using App9.Services;
+using App9.Views;
+using OAuthNativeFlow;
+
+namespace App9
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new NavigationPage( new OAuthNativeFlowPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
